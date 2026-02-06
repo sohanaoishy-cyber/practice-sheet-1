a42df0a96e30a39ae0652f044ba75b94c23a44ef
sohanaoishy-cyber <sohana.oishy@auw.edu.bd>
2026-02-06 21:09:19 +0600

    Add files via upload

---
+++ LOOP14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n, count = 0;
+    scanf("%d", &n);
+    if (n == 0)
+        count = 1;
+    while (n != 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}
