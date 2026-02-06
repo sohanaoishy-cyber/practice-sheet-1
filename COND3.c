74913b291a1c54991e84c0c9ce87f654c821ad7a
sohanaoishy-cyber <sohana.oishy@auw.edu.bd>
2026-02-06 21:01:22 +0600

    Add files via upload

---
+++ COND3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a > b)
+        printf("%d\n", a);
+    else if (b > a)
+        printf("%d\n", b);
+    else
+        printf("Equal\n");
+    return 0;
+}
